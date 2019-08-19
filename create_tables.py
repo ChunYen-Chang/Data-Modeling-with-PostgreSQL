fab0d85a3e0d965d26226174247f75ce33aebbd0
ChunYen-Chang <fikrusnk@gmail.com>
2019-08-19 08:51:34 +0000

    DOCS: Write docstring for each function and print() in main function
    The previous version forgets to write the docstring for each function.
    This make other developer hardly understand the code. This version fix
    this issue. Also, this version puts print statement in main functuon.
    It allows users to understand which stage this code is operating when
    they run this code in terminal.

--- create_tables.py
+++ create_tables.py
@@ -1,8 +1,16 @@
+# import the packages which are needed in this file
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 
+# define functions
 def create_database():
+    """
+    Description: 
+        This function helps us to create a connection object and cursor object for connecting
+        to Postgres.
+    Parameters: None
+    """
     # connect to default database
     conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
     conn.set_session(autocommit=True)
@@ -23,22 +31,37 @@ def create_database():
 
 
 def drop_tables(cur, conn):
+    """
+    Description: drop tables in Postgres database if they exist    
+    Parameters:
+        -conn: connection object (with Postgres)
+        -cur: cursor object
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Description: create tables in Postgres database   
+    Parameters:
+        -conn: connection object (with Postgres)
+        -cur: cursor object
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    print('connect to Postgres...')
     cur, conn = create_database()
     
+    print('create tables...')
     create_tables(cur, conn)
 
+    print('close connection...')
     conn.close()
 
 
